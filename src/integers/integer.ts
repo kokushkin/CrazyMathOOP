b0f88a72293d2e53dc239915e5d77b901fcafb70
Nickolay Kulikov <fipsiksi@yandex.ru>
2021-01-10 01:07:24 +0200

    add  flch

--- src/integers/integer.ts
+++ src/integers/integer.ts
@@ -1,110 +1,79 @@
-export class Integer {
-  public integer: number;
-
-  constructor(integer: number) {
-    this.integer = integer;
-  }
-
-  public plus(anotherInt: Integer) {
-    return new Integer(this.integer + anotherInt.integer);
-  }
-
-  public minus(anotherInt: Integer) {
-    return new Integer(this.integer - anotherInt.integer);
-  }
-
-  public times(anotherInt: Integer) {
-    return new Integer(this.integer * anotherInt.integer);
-  }
-
-  public equal(anotherInt: Integer) {
-    return this.integer === anotherInt.integer;
-  }
-
-  public divides(anotherInteger: Integer, anotherOneInteger: Integer) {
-    return anotherInteger.equal(this.times(anotherOneInteger));
-  }
-
-  public more(anotherInt: Integer) {
-    return this.integer > anotherInt.integer;
-  }
-
-  public less(anotherInt: Integer) {
-    return this.integer < anotherInt.integer;
-  }
-
-  public mod() {
-    if (this.less(new Integer(0))) return new Integer(0).minus(this);
-    return this;
-  }
-
-  public IfIntegerDividesTwoOthersThemItDividesTheCombination(
-    d: Integer,
-    x: Integer,
-    y: Integer,
-    r: Integer,
-    s: Integer,
-    a: Integer,
-    b: Integer
-  ) {
-    const combinations = a.plus(x).plus(b.times(y));
-    const regroup = a.times(r.times(d)).plus(b.times(s.times(d)));
-    const dDivideSum = d.times(a.times(r).plus(b.times(s)));
-
-    if (d.divides(x, r) && d.divides(y, s)) {
-      return combinations === regroup && regroup === dDivideSum;
-    }
-    return false;
-  }
-
-  public theorem1_1_1(n: Integer, m: Integer, all: Integer[]) {
-    if (!m.more(new Integer(0))) {
-      throw new Error("m must be more then 0");
-    }
-
-    const specialForms: Integer[] = [];
-    all.forEach((s) => {
-      const form = n.minus(s.times(m));
-      if (!form.less(new Integer(0))) {
-        specialForms.push(form);
-      }
-    });
-
-    if (specialForms.length === 0) {
-      throw new Error("Can't be empty. WHY??");
-    }
-
-    const r = specialForms.sort()[0];
-
-    if (!r.less(m.mod())) {
-      if (
-        !(
-          r.minus(m.mod()).more(new Integer(0)) ||
-          r.minus(m.mod()).equal(new Integer(0))
-        )
-      ) {
-        throw new Error("Impossible");
-      }
-    }
-
-    return {};
+function flch(expression: boolean) {
+  if (!expression) {
+    throw new Error("Math doesn't work");
   }
 }
 
-export class ReductionModulo {
-  public n: Integer;
-  public q: Integer;
-  public m: Integer;
-  public r: Integer;
+function mod(n: number) {
+  if (n < 0) {
+    return -n;
+  } else {
+    return n;
+  }
+}
 
-  constructor(n: Integer, q: Integer, m: Integer, r: Integer) {
-    this.n = n;
-    this.q = q;
-    this.m = m;
-    this.r = r;
+function divides(
+  oneInteger: number,
+  anotherInteger: number,
+  OneAnotherInteger: number
+) {
+  return anotherInteger === oneInteger * OneAnotherInteger;
+}
+
+function ifDevidesTwoThenDevidesTheirCombination(
+  d: number,
+  x: number,
+  y: number,
+  r: number,
+  s: number,
+  a: number,
+  b: number
+) {
+  /// requirements
+  if (!(divides(d, x, r) && divides(d, y, s))) {
+    throw new Error("This is initial conditions");
+  }
+  /// end of requirements
+
+  flch(
+    a * x + b * y === a * r * d + b * s * d &&
+      a * r * d + b * s * d === d * (a * r + b * s)
+  );
+
+  return divides(d, a * x + b * y, a * r + b * s) === true;
+}
+
+function theorem1_1_1(n: number, m: number, all: number[]) {
+  /// requirements
+  if (!(m > 0)) {
+    throw new Error("m must be more then 0");
+  }
+  /// end of requirements
+
+  const specialForms: number[] = [];
+  all.forEach((s) => {
+    const form = n - s * m;
+    if (form >= 0) {
+      specialForms.push(form);
+    }
+  });
+
+  if (specialForms.length === 0) {
+    throw new Error("Can't be empty. WHY??");
   }
 
-  public definition() {
-    return this.n.integer === this.q.integer * this.m.integer + this.r.integer;
+  const r = specialForms.sort()[0];
+  const q = -(r - n) / m;
+  flch(r === n - q * m);
+
+  if (!(r < mod(m))) {
+    flch(r >= mod(m) && r - mod(m) >= 0);
+
+    flch(
+      r - mod(m) === n - q * m - mod(m) &&
+        n - q * m - mod(m) === n - (q - 1) * m
+    );
   }
+
+  return {};
 }
